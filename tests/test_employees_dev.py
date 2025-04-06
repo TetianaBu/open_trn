d0c3cfcb527a3f4f58ed16b5718bd0cb0a1accc4
tetianaBu <tetianataker@gmail.com>
2025-04-06 19:28:40 +0200

    Add new test with develop

---
+++ tests/test_employees_dev.py
@@ -0,0 +1,20 @@
+import pytest
+import re
+from config.db_config import get_db_connection
+
+def test_count_emails_2():
+    """
+    Validate emails count
+    """
+    conn = get_db_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT Count(email) FROM hr.employees")
+        result = cursor.fetchall()
+    finally:
+        conn.close()
+
+    emails = [row[0] for row in result]
+
+
+    assert emails[0] == 40, f"Count equals: {emails}"
